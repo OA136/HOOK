57f2fe6ca835ed1dcdf753fe6b07ba1fcb624666
OA136 <1803067851@qq.com>
2017-04-12 10:48:14 +0800

    netlink

--- kernel_mode/kernel_netlink.c
+++ kernel_mode/kernel_netlink.c
@@ -3,7 +3,7 @@
 #include <linux/kernel.h>
 #include <linux/skbuff.h>
 #include <linux/string.h>
-
+#include < net/sock.h>
 #include <linux/netlink.h>
 
 MODULE_LICENSE("GPL");
@@ -11,14 +11,25 @@ MODULE_AUTHOR("SQ");
 MODULE_DESCRIPTION("Communicate with user");
 MODULE_VERSION("1.0");
 
-void control() {
-    struct net *net = (struct net*)kmalloc(sizeof(struct net), GFP_ATOMIC);
-    struct sock *skd = netlink_kernel_create(net, NETLINK_NETFILTER, );
 
+struct sock *skd = NULL;
+
+void receive(struct sk_buff *skb) {
+    printk(KERN_ALERT "Kernel received message!\n");
 }
 
+
+
 static int init(void) {
-    control();
+    struct netlink_kernel_cfg cfg = {
+
+        .input = receive,
+    };
+    skd = netlink_kernel_create(net, NETLINK_NETFILTER, &cfg);
+    if (!skd) {
+        printk(KERN_ALERT "Error create sock!\n");
+        return -1;
+    }
     return 0;
 }
 
